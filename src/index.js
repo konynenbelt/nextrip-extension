3637e8bf237dbfd783f1b836c0626754327a4e7d
jordankonynenbelt <jordankonynenbelt@gmail.com>
2018-04-06 02:09:33 -0500

    use bootstrap cdn instead of megafile

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TripList from './components/TripList.js';
 import NexTripForm from './components/NexTripForm.js';
-import css from './index.css';
 
 class App extends React.Component {
     // TODO: Make saved trips persistent using chrome extension storage.
